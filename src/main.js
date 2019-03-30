3266d1d7d04e0c2fd9ca0f62c571322a34e01f02
Nancy <lexyfeer@hotmail.com>
2019-03-30 10:41:09 -0600

    solucionando conflictos1.1

--- src/main.js
+++ src/main.js
@@ -1,30 +1,30 @@
 'use strict'
 
-// window.addEventListener("load", ()=>{
+window.addEventListener("load", ()=>{
 //     console.log("mi js funciona despues de que termino de cargar mi html completo")
-
+window.datosmujeres = {
+    datawoman: datawoman
+  
+  };
+  
+  
+  function datawoman(MUJERES){
+    let name=[];
+    for(let i=0;i<MUJERES.length; i++){
+      let pruebadata={
+        "nombre": MUJERES[i].Nombre,
+        
+      };
+      
+      name.push(pruebadata); 
+      console.log(name);
+    }
+    return name;
+  }
+  
+  //esta funcion aun no sirve, estoy tratando de acceder a la info de la base de datos.
 
   
-// });
-
-window.datosmujeres = {
-  datawoman: datawoman
-
-};
+});
 
 
-function datawoman(MUJERES){
-  let name=[];
-  for(let i=0;i<MUJERES.length; i++){
-    let pruebadata={
-      "nombre": MUJERES[i].Nombre,
-      
-    };
-    
-    name.push(pruebadata); 
-    console.log(name);
-  }
-  return name;
-}
-
-//esta funcion aun no sirve, estoy tratando de acceder a la info de la base de datos.
