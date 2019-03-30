67092314754a5776c25ac306b159ae45c9905d0c
Nancy <lexyfeer@hotmail.com>
2019-03-30 10:37:46 -0600

    solucionando conflictos1

--- src/main.js
+++ src/main.js
@@ -1,41 +1,30 @@
 'use strict'
 
-window.addEventListener("load", ()=>{
-    // console.log("mi js funciona despues de que termino de cargar mi html completo")
+// window.addEventListener("load", ()=>{
+//     console.log("mi js funciona despues de que termino de cargar mi html completo")
 
-    // function loadJASON () {
-//     fetch('datosmujeres.json')
-//     .then(function (res) {
-//         return res.json();
-//     })
-//     .then(function (data) {
-//         console.log(data);
-//     });
-// };
-// loadJASON();
+
+  
+// });
+
 window.datosmujeres = {
-    datawoman: datawoman
-  
-  };
-  
-  
-  function datawoman(MUJERES){
-    let name=[];
-    for(let i=0;i<MUJERES.length; i++){
-      let pruebadata={
-        "nombre": MUJERES[i].Nombre,
-        
-      };
+  datawoman: datawoman
+
+};
+
+
+function datawoman(MUJERES){
+  let name=[];
+  for(let i=0;i<MUJERES.length; i++){
+    let pruebadata={
+      "nombre": MUJERES[i].Nombre,
       
-      name.push(pruebadata); 
-      console.log(name);
-    }
-    return name;
+    };
+    
+    name.push(pruebadata); 
+    console.log(name);
   }
-  
-  //esta funcion aun no sirve, estoy tratando de acceder a la info de la base de datos.
+  return name;
+}
 
-
-  
-});
-
+//esta funcion aun no sirve, estoy tratando de acceder a la info de la base de datos.
